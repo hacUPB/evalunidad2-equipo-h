19ee87b030eed3d0dfd6714b7241cd293845a423
SamuelElHG <samuel.hernandezg@upb.edu.co>
2022-09-27 19:43:07 -0500

    push inicial

--- problem1/eventList.c
+++ problem1/eventList.c
@@ -5,26 +5,62 @@
 
 EventList *CreateEventList(void)
 {
-
+    EventList *eventList = malloc(sizeof(EventList));
+    eventList->head = NULL;
+    eventList->last = NULL;
+    eventList->isEmpty = 1;
+    return eventList;
 }
 
 void DestroyEventList(EventList *this)
 {
-
+    free(this);
 }
 
 Event *SearchEvent(EventList *this, char *name)
 {
-
+    Event *crntEvent = this->head;
+    if (this->isEmpty == 0)
+    {
+        while (crntEvent != NULL)
+        {
+            if (strcmp(name, crntEvent->eventName)==0)
+            return crntEvent;
+            crntEvent = crntEvent->next;
+        }
+    }
+    crntEvent = NULL;
+    return crntEvent;
 }
 
 void AddEvent(EventList *this, Event *event)
-{
+{    
+     Event *crntEvent = this->head;
+     while (crntEvent != NULL)
+     {
+         if(strcmp(event->eventName, crntEvent->eventName) == 0)
+         {
+            return;
+         }
+         crntEvent = crntEvent->next;
+     }
 
+     if(this->isEmpty == 0)
+     {
+          this->last->next = event;
+          this->last=event;
+     }
+     else
+     {
+         this->head = event;
+         this->last = event;
+         this->isEmpty = 0;    
+     }
 }
 
 void RemoveEvent(EventList *this, char *name)
 {
+
 }
 
 void ListEvents(EventList *this)
