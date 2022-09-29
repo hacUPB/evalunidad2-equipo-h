4400cc01b7b1cbd02a1b107fb4fb9895e31f06cb
Sebastian Ayala <sebas.ayala2002@hotmail.com>
2022-09-29 16:05:40 -0500

    Avance

--- problem1/event.c
+++ problem1/event.c
@@ -5,7 +5,6 @@
 Event *CreateEvent(char *name)
 {
     Event *event = malloc(sizeof(Event));
-    name[15] = 0;
     strncpy(event->eventName, name, strlen(name)+ 1);
     event->next = NULL;
     return event;
@@ -14,5 +13,4 @@ Event *CreateEvent(char *name)
 void DestroyEvent(Event *this)
 {
     free(this);
-
 }
--- problem1/eventList.c
+++ problem1/eventList.c
@@ -14,56 +14,106 @@ EventList *CreateEventList(void)
 
 void DestroyEventList(EventList *this)
 {
-    free(this);
+    for (int i = 0; i < strlen(this); i++)
+    {
+        free(this);
+        this ++;
+    }
 }
 
 Event *SearchEvent(EventList *this, char *name)
 {
-    Event *crntEvent = this->head;
+    Event *actEvent = this->head;
     if (this->isEmpty == 0)
     {
-        while (crntEvent != NULL)
+        while (actEvent != NULL)
         {
-            if (strcmp(name, crntEvent->eventName)==0)
-            return crntEvent;
-            crntEvent = crntEvent->next;
+            if (strcmp(name, actEvent->eventName) == 0)
+                return actEvent;
+            actEvent = actEvent->next;
         }
     }
-    crntEvent = NULL;
-    return crntEvent;
+    actEvent = NULL;
+    return actEvent;
 }
 
 void AddEvent(EventList *this, Event *event)
-{    
-     Event *crntEvent = this->head;
-     while (crntEvent != NULL)
-     {
-         if(strcmp(event->eventName, crntEvent->eventName) == 0)
-         {
+{
+    Event *actEvent = this->head;
+    while (actEvent != NULL)
+    {
+        if (strcmp(event->eventName, actEvent->eventName) == 0)
+        {
             return;
-         }
-         crntEvent = crntEvent->next;
-     }
+        }
+        actEvent = actEvent->next;
+    }
 
-     if(this->isEmpty == 0)
-     {
-          this->last->next = event;
-          this->last=event;
-     }
-     else
-     {
-         this->head = event;
-         this->last = event;
-         this->isEmpty = 0;    
-     }
+    if (this->isEmpty == 0)
+    {
+        this->last->next = event;
+        this->last = event;
+    }
+    else
+    {
+        this->head = event;
+        this->last = event;
+        this->isEmpty = 0;
+    }
 }
 
 void RemoveEvent(EventList *this, char *name)
 {
-
+    if (this->isEmpty == 0)
+    {
+        Event *actEvent = this->head->next;
+        Event *antEvent = this->head;
+        if (strcmp(name, antEvent->eventName) == 0)
+        {
+            if (antEvent->next == NULL)
+            {
+                this->head = NULL;
+                this->last = NULL;
+                this->isEmpty = 1;
+                DestroyEvent(antEvent);
+            }
+            else
+            {
+                this->head = actEvent;
+                DestroyEvent(antEvent);
+            }
+        }
+        while (actEvent != NULL)
+        {
+            if (strcmp(name, actEvent->eventName) == 0)
+            {
+                antEvent->next = actEvent->next;
+                if (this->last->next == NULL)
+                {
+                    this->last = antEvent;
+                    DestroyEvent(actEvent);
+                }
+            }
+            antEvent = actEvent;
+            actEvent = actEvent->next;
+        }
+    }
 }
 
 void ListEvents(EventList *this)
 {
+    if (this->isEmpty == 0)
+    {
+        Event *actEvent = this->head;
 
+        while (actEvent != NULL)
+        {
+            printf("%s\n", actEvent->eventName);
+            actEvent = actEvent->next;
+        }
+    }
+    else
+    {
+        printf("empty\n");
+    }
 }
